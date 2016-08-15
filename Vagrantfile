96b25d262489607f5a480bcd970c52756d240cc9
lufinkey <luisfinke@gmail.com>
2016-08-14 22:08:02 -0400

    Made openvpn auto-start in VM

--- Vagrantfile
+++ Vagrantfile
@@ -13,6 +13,7 @@ Vagrant.configure(2) do |config|
 
 	config.vm.synced_folder "./shared", "/shared"
 
-	config.vm.provision "file", source: "scripts/whatismyip", destination: "/usr/local/bin/whatismyip"
+	config.vm.provision "file", source: "tools", destination: "/tmp/tools"
+
 	config.vm.provision "shell", path: "scripts/vagrant-setup.sh"
 end
--- scripts/vagrant-setup.sh
+++ scripts/vagrant-setup.sh
@@ -2,6 +2,11 @@
 if [ ! -f "/shared/.setup_done" ]
 then
 	sudo apt-get install -y aria2 unzip openvpn
+	sudo cp -rf /tmp/tools/* /usr/local/bin
+	sudo rm -rf /tmp/tools
+
+	sudo service openvpn stop
+
 	cd /etc/openvpn
 	if [ ! -f openvpn.zip ]
 	then
@@ -10,9 +15,22 @@ then
 	fi
 	sudo rm -rf "/etc/openvpn/auth.txt"
 	sudo mv "/shared/tmp/.pia_credentials" "/etc/openvpn/auth.txt"
-	sudo openvpn --config "US East.ovpn" --auth-user-pass "auth.txt" --daemon
+
+	openvpn_prefs=$(
+	{
+		cat <<EOF
+AUTOSTART="all"
+OPTARGS=""
+OMIT_SENDSIGS=0
+EOF
+	})
+	sudo echo "$openvpn_prefs" > /etc/default/openvpn
+	
+	openvpn_conf="torrentbox.conf"
+
+	cat "US East.ovpn" | sed -e 's/^auth-user-pass.*//' > "$openvpn_conf"
+	echo 'auth-user-pass "auth.txt"' >> "$openvpn_conf"
+
 	touch "/shared/.setup_done"
-else
-	cd /etc/openvpn
-	sudo openvpn --config "US East.ovpn" --auth-user-pass "auth.txt" --daemon
 fi
+
